80ec07e3eaf1f45796ee324617a5314e95d6770c
Gerard KESSE <tiakagerard@hotmail.com>
2017-09-14 02:37:06 +0200

    Initial Commit

---
+++ Fonction_Lambda/src/GMain.py
@@ -0,0 +1,8 @@
+#================================================
+square = lambda x: x*x
+
+print("square(3) : ", square(3))
+print("square(5) : ", square(5))
+print("square(7) : ", square(7))
+print("square(9) : ", square(9))
+#================================================
