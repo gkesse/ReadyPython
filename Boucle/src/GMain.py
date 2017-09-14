348532e9a3fa686e02bbdafa2ff76afc28a0ee0e
Gerard KESSE <tiakagerard@hotmail.com>
2017-09-14 02:23:29 +0200

    Initial Commit

---
+++ Boucle/src/GMain.py
@@ -0,0 +1,12 @@
+#================================================
+m_int = 1234
+m_double = 12.34
+m_string = "Hello World !"
+m_format = ""
+
+m_format += "m_int : " + str(m_int) + "\n"
+m_format += "m_double : " + str(m_double) + "\n"
+m_format += "m_string : " + m_string + "\n"
+
+print(m_format)
+#================================================
---
+++ Interface_Graphique/win/cmd_run.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set PATH=C:\dev\Python
+
+set GSRC=../src
+
+echo.
+python %GSRC%/GMain.py
+echo.
