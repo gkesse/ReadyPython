1ef6124792a1dada09d977f10caba51b71c2153c
Gerard KESSE <tiakagerard@hotmail.com>
2021-01-08 00:27:46 +0100

    Initial Commit

--- win/env.bat
+++ win/env.bat
@@ -1,7 +1,8 @@
 @echo off
 ::===============================================
 set "PATH=C:\MinGW\bin;%PATH%"
-set "PATH=C:\Python27;%PATH%"
+set "PATH=C:\Python38;%PATH%"
+set "PATH=C:\Python38\Scripts;%PATH%"
 ::===============================================
 set "GPROJECT_ROOT=C:\Users\Admin\Downloads\Programs"
 set "GPROJECT_PATH=%GPROJECT_ROOT%\ReadyPython"
@@ -15,10 +16,12 @@ set "GVIDEO_PATH=%GDATA_PATH%\video"
 set "GGIT_URL=https://github.com/gkesse/ReadyPython.git"
 set "GGIT_NAME=ReadyPython"
 ::===============================================
-set "GPIP_PACKAGE_NAME=pafy"
-set "GPIP_PACKAGE_NAME=youtube-dl"
-set "GPIP_PACKAGE_NAME=db-sqlite3"
-set "GPIP_PACKAGE_NAME=pip"
-set "GPIP_PACKAGE_NAME=wheel"
-set "GPIP_PACKAGE_NAME=pyinstaller"
+set "GPIP_PACKAGE_NAME="
+set "GPIP_PACKAGE_NAME=%GPIP_PACKAGE_NAME% pip"
+set "GPIP_PACKAGE_NAME=%GPIP_PACKAGE_NAME% pafy"
+set "GPIP_PACKAGE_NAME=%GPIP_PACKAGE_NAME% youtube-dl"
+set "GPIP_PACKAGE_NAME=%GPIP_PACKAGE_NAME% db-sqlite3"
+set "GPIP_PACKAGE_NAME=%GPIP_PACKAGE_NAME% wheel"
+set "GPIP_PACKAGE_NAME=%GPIP_PACKAGE_NAME% pyinstaller"
+set "GPIP_PACKAGE_NAME=%GPIP_PACKAGE_NAME% pyqt5"
 ::===============================================
